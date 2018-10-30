005e7733f754276f711f24b4d53328eaf8dc0c57
Andrew Rogala <adrogala87@gmail.com>
2018-10-30 18:42:27 -0400

    add better nav structure

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,17 @@ import icon from '../img/tabIcon.png';
 
 import '../styles/main.scss';
 
+function toggleNav() {
+	const toggleNav = document.getElementById('navi-toggle').checked;
+	//console.log(toggleNav);
+	if(toggleNav === false) {
+		document.getElementById('navi-toggle').checked = true;
+	}
+	else {
+		document.getElementById('navi-toggle').checked = false;
+	}
+}
+
 export default ({children}) => (
 	<StaticQuery
 		query={graphql`
@@ -32,21 +43,24 @@ export default ({children}) => (
 		  			<link rel="shortcut icon" href={icon} />
 		  			<link href="https://fonts.googleapis.com/css?family=Mali:500,700" rel="stylesheet" />
 				</Helmet>
-				<header>
-					<ul>
-						<li>
-							<Link to="/">Home</Link>
-						</li>
-						<li>
-							<Link to="/portfolio">Portfolio</Link>
-						</li>
-						<li>
-							<Link to="/techblog">Tech Blog</Link>
-						</li>
-						<li>
-							<Link to="/about">About Me</Link>
-						</li>
+
+				<nav className="nav">
+					<input type="checkbox" className="nav__checkbox" id="navi-toggle" />
+					<label htmlFor="navi-toggle" className="nav__button" aria-haspopup="true" role="button" tabIndex="0" aria-label="navigation menu" onKeyPress={()=>toggleNav()}>
+						<span className="nav__icon">&nbsp;</span>
+					</label>
+					<ul className="nav__list">
+						<div className="nav__item-container">
+							<li className="nav__item-1"><Link to="/"          className="nav__link">Home</Link></li>
+							<li className="nav__item-2"><Link to="/portfolio" className="nav__link">Portfolio</Link></li>
+							<li className="nav__item-3"><Link to="/techblog"  className="nav__link">Tech Blog</Link></li>
+							<li className="nav__item-4"><Link to="/about"    className="nav__link">About</Link></li>
+						</div>
 					</ul>
+				</nav>
+
+				<header>
+
 				</header>
 				{children}
 				<footer>

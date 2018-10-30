c0cb071d8338aec69ac2cae4e95442a3147e3046
Andrew Rogala <adrogala87@gmail.com>
2018-10-30 15:03:14 -0400

    render about from markdown

---
+++ src/about/about.md
@@ -0,0 +1,9 @@
+---
+path: src/about/
+title: About Me
+date: 2018-10-30
+author: Andrew Rogala
+---
+
+<h3>Welcome</h3>
+<P>Hi I am glad you found me. I first discovered my interest in programming and computer science in 2014 during an academic year of Java at Montclair State University. Since then I never really stopped learning about programming and computers. I have tinkered with Python and the Raspberry Pi, learned some statistical programming with R in a data mining course, assembled my dual boot gaming pc, written a few basic Linux shell scripts, and even picked up a little Excel VBA at work to automate some calculations.</P>
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 
-function About() {
+function About({data}) {
+	//console.log(data.allMarkdownRemark.edges[0].node);
+	const content = data.allMarkdownRemark.edges[0].node
 	return (
 		<Layout>
-			<div>This is my about page</div>
+			<div dangerouslySetInnerHTML={{ __html: content.html }}></div>
 		</Layout>
 	);
 }
 
 export default About;
+
+//query just the about me page
+export const query = graphql`
+	query {
+		allMarkdownRemark(filter: {frontmatter: {title: {eq:"About Me"}}}) {
+			edges {
+				node {
+					html
+				}
+			}
+		}
+	}
+`

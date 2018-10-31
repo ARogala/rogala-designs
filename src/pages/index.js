45346f24a3664ac543858666d9cc5ed21530e0ff
Andrew Rogala <adrogala87@gmail.com>
2018-10-31 15:09:36 -0400

    add index.js style sheet

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,11 @@ function Home({data}) {
 	return (
 		<Layout>
 			<div>
-				<p>Hi, My name is Andrew! I am a Front-End Web developer specializing in
-					React JavaScript and the JAM (JavaScript API's and Markdown) Stack.
-					I love making custom web sites for businesses or individuals.
-					Contact me if you need a business page or custom personal blog.</p>
+				<p className="typewriter">Hi, My name is Andrew! I am a Front-End Web developer specializing in
+				React JavaScript and the JAM (JavaScript API's and Markdown) Stack.
+				I love making custom web sites, blogs, and web apps.
+				Check out some of my work below.</p>
+				<h3>Featured Work</h3>
 			</div>
 		</Layout>
 	);

0b4e56818d20025c7bbe2d90f2f7cc4cf1f505c8
Andrew Rogala <adrogala87@gmail.com>
2018-10-31 14:15:48 -0400

    style header

--- src/components/layout.js
+++ src/components/layout.js
@@ -43,26 +43,25 @@ export default ({children}) => (
 		  			<link rel="shortcut icon" href={icon} />
 		  			<link href="https://fonts.googleapis.com/css?family=Mali:500,700" rel="stylesheet" />
 				</Helmet>
-
-				<nav className="nav">
-					<input type="checkbox" className="nav__checkbox" id="navi-toggle" />
-					<label htmlFor="navi-toggle" className="nav__button" aria-haspopup="true" role="button" tabIndex="0" aria-label="navigation menu" onKeyPress={()=>toggleNav()}>
-						<span className="nav__icon">&nbsp;</span>
-					</label>
-					<ul className="nav__list">
-						<div className="nav__item-container">
-							<li className="nav__item-1"><Link to="/"          className="nav__link">Home</Link></li>
-							<li className="nav__item-2"><Link to="/portfolio" className="nav__link">Portfolio</Link></li>
-							<li className="nav__item-3"><Link to="/techblog"  className="nav__link">Tech Blog</Link></li>
-							<li className="nav__item-4"><Link to="/about"    className="nav__link">About</Link></li>
-						</div>
-					</ul>
-				</nav>
-
 				<header className="header">
-					<h1>Rogala Designs</h1>
-					<h2>Front-End Web Development</h2>
-
+					<nav className="nav">
+						<input type="checkbox" className="nav__checkbox" id="navi-toggle" />
+						<label htmlFor="navi-toggle" className="nav__button" aria-haspopup="true" role="button" tabIndex="0" aria-label="navigation menu" onKeyPress={()=>toggleNav()}>
+							<span className="nav__icon">&nbsp;</span>
+						</label>
+						<ul className="nav__list">
+							<div className="nav__item-container">
+								<li className="nav__item-1"><Link to="/"          className="nav__link">Home</Link></li>
+								<li className="nav__item-2"><Link to="/portfolio" className="nav__link">Portfolio</Link></li>
+								<li className="nav__item-3"><Link to="/techblog"  className="nav__link">Tech Blog</Link></li>
+								<li className="nav__item-4"><Link to="/about"    className="nav__link">About</Link></li>
+							</div>
+						</ul>
+					</nav>
+					<div className="header__titleBox">
+						<h1 className="header__title-1">{data.site.siteMetadata.title}</h1>
+						<h2 className="header__title-2">Front-End Web Development</h2>
+					</div>
 				</header>
 				{children}
 				<footer>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,9 @@ function Home({data}) {
 		<Layout>
 			<div>
 				<p>Hi, My name is Andrew! I am a Front-End Web developer specializing in
-				React JavaScript and the JAM (JavaScript API's and Markdown) Stack.
-				I love making custom web sites for businesses or individuals.
-				Contact me if you need a business page or custom personal blog.</p>
+					React JavaScript and the JAM (JavaScript API's and Markdown) Stack.
+					I love making custom web sites for businesses or individuals.
+					Contact me if you need a business page or custom personal blog.</p>
 			</div>
 		</Layout>
 	);

73883b928f4ad16c6413640e567ec504e142656f
Andrew Rogala <adrogala87@gmail.com>
2018-11-02 11:53:28 -0400

    add portfolio md

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 
-function Portfolio() {
+function Portfolio({data}) {
+	//console.log(data.allMarkdownRemark.edges[0].node);
+	const content = data.allMarkdownRemark.edges[0].node;
 	return (
 		<Layout>
-			<div>This is my portfolio page</div>
+			<h3 className="heading-tertiary">Portfolio</h3>
+			<div dangerouslySetInnerHTML={{ __html: content.html }}></div>
 		</Layout>
 	);
 }
 
 
 export default Portfolio;
+
+//query just the featured work content
+export const query = graphql`
+	query {
+		allMarkdownRemark(filter: {frontmatter: {title: {eq:"Portfolio"}}}) {
+			edges {
+				node {
+					html
+				}
+			}
+		}
+	}
+`
---
+++ src/portfolio/portfolio.md
@@ -0,0 +1,6 @@
+---
+path: src/portfolio/
+title: Portfolio
+date: 2018-11-02
+author: Andrew Rogala
+---

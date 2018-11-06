f46b56889f1a5014013d26c59a84b5b9e30575b4
Andrew Rogala <adrogala87@gmail.com>
2018-11-06 12:33:54 -0500

    remove sorted blog list from tech blog page

--- src/pages/techblog.js
+++ src/pages/techblog.js
@@ -8,58 +8,6 @@ import blogImg from '../img/blogPostImage.jpg';
 
 function TechBlog({data}) {
 	//console.log(data);
-	//add category to edges so nodes(blog posts) can be sorted by category
-	data.allMarkdownRemark.edges.map(({node}, index) => {
-		return data.allMarkdownRemark.edges[index].category = node.frontmatter.category + ' - ' + node.frontmatter.subCategory;
-	});
-
-	const groupedPosts = groupBy(data.allMarkdownRemark.edges, 'category');
-	//console.log(groupedPosts);
-	const allCategories = Object.keys(groupedPosts);
-	//console.log(allCategories);
-
-	/*
-		for each post category if the number of posts is greater than 1
-		build the DOM
-	*/
-	const dropDownUL = [];
-	for(let i = 0; i < allCategories.length; i++ ) {
-		if(groupedPosts[allCategories[i]].length > 1) {
-			//build the dropDownUL
-			dropDownUL.push(
-				<li key={i} className="dropdown">
-					<span className="dropdown__btn">{allCategories[i]}:</span>
-					<ul aria-label="submenu" className="dropdown__ul">
-						{groupedPosts[allCategories[i]].map((post) => {
-							//console.log(post.node);
-							return (
-								<li key={post.node.id}>
-									<Link to={post.node.fields.slug}>
-										{post.node.frontmatter.title}
-									</Link>
-								</li>
-							);
-						})}
-					</ul>
-				</li>
-			);
-		}
-	}
-
-	//build the DOM for the categories with one post
-	const singlePost = [];
-	for(let i = 0; i < allCategories.length; i++) {
-		if(groupedPosts[allCategories[i]].length === 1) {
-			//console.log(groupedPosts[allCategories[i]][0]);
-			singlePost.push(
-				<li key={groupedPosts[allCategories[i]][0].node.id}>
-					<Link to={groupedPosts[allCategories[i]][0].node.fields.slug}>
-						{groupedPosts[allCategories[i]][0].node.frontmatter.title}
-					</Link>
-				</li>
-			);
-		}
-	}
 	let recentPosts = data.allMarkdownRemark.edges.slice(0,10);
 	return (
 		<Layout>
@@ -79,13 +27,8 @@ function TechBlog({data}) {
 							<p>Excerpt: {node.excerpt}</p>
 						</div>
 					</div>
-
 				))}
-				<h2>Archived Posts: {data.allMarkdownRemark.totalCount}</h2>
-				<span>Multiple Posts in SubCategory:</span>
-				{dropDownUL}
-				<span>Single Post in SubCategory:</span>
-				{singlePost}
+				<Link to='/techblog-archive'>Blog Archive: {data.allMarkdownRemark.totalCount} Posts</Link>
 			</div>
 		</Layout>
 	);
@@ -109,7 +52,6 @@ export const query = graphql`
 						subCategory
 					}
 					excerpt(pruneLength: 70)
-					html
 					fields {
 						slug
 					}

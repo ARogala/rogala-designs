d96c72c4a836a2ed6707cb0950ca5bc3dfdfd4b2
Andrew Rogala <adrogala87@gmail.com>
2018-11-07 14:14:37 -0500

    restructure promise in node

--- gatsby-node.js
+++ gatsby-node.js
@@ -49,40 +49,50 @@ Map the query results to pages
 exports.createPages = ({ graphql, actions }) => {
 	const { createPage } = actions;
 	return new Promise((resolve, reject) => {
-		graphql(`
-		  {
-		    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC } filter: { frontmatter: { category: {ne: null}}}) {
-		      edges {
-		        node {
-		          fields {
-		            slug
-		          }
-		          frontmatter {
-		          	title
-		          }
-		        }
-		      }
-		    }
-		  }
-		`
-		).then(result => {
-			const posts = result.data.allMarkdownRemark.edges;
-			posts.forEach(({node}, index) => {
+		resolve(
+			graphql(
+				`
+					{
+						allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC } filter: { frontmatter: { category: {ne: null}}}) {
+							edges {
+								node {
+									fields {
+										slug
+									}
+									frontmatter {
+										title
+									}
+								}
+							}
+						}
+					}
+				`
+			).then(result => {
+				if(result.errors) {
+					console.log(result.errors);
+					reject(result.errors);
+				}
+
+				// Create blog posts pages.
+				const posts = result.data.allMarkdownRemark.edges;
+				posts.forEach(({node}, index) => {
 				const previous = index === posts.length - 1 ? null : posts[index + 1].node;
           		const next = index === 0 ? null : posts[index - 1].node;
-				createPage({
-					path: node.fields.slug,
-					component: path.resolve('./src/templates/blog-post.js'),
-					context: {
-						// Data passed to context is available
-            			// in page queries as GraphQL variables.
-            			slug: node.fields.slug,
-            			previous,
-            			next
-					}
+					createPage({
+						path: node.fields.slug,
+						component: path.resolve('./src/templates/blog-post.js'),
+						context: {
+							// Data passed to context is available
+	            			// in page queries as GraphQL variables.
+	            			slug: node.fields.slug,
+	            			previous,
+	            			next
+						}
+					});
 				});
-			});
-			resolve();
-		})
+
+			})
+		);
 	});
 }
+

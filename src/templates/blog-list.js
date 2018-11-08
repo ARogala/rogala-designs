adbd7ced0274c19642b9ac37170035d96e75f68d
Andrew Rogala <adrogala87@gmail.com>
2018-11-08 14:07:45 -0500

    add intro paragraph to techblog page

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -16,6 +16,14 @@ class BlogList extends React.Component {
 			<Layout>
 				<div className="techblog">
 					<h1 className="techblog__title">Web Development Blog</h1>
+					<p>Welcome to my blog and thanks for visiting! I hope you learn a lot here
+					and enjoy the knowledge shared throughout the course of my career in
+					Front-End Web Development. The main topics covered will be JavaScript,
+					ReactJS, GatsbyJS, CSS, SASS, HTML, and anything else I may learn,
+					experiment with, or find interesting. This blog is also serving as my personal documentation
+					of the JavaScript language as well as any information and tutorials that
+					I feel will benefit "future me" when I inevitably forget how something works.
+					So, read on and hope you enjoy my notes, tips, and tricks of the trade!!</p>
 					<h2 className="techblog__recent">Recent Posts:</h2>
 					{posts.map(({node}) => (
 						<div key={node.id} className="techblog__post">

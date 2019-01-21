914ed27562bfeb3ebd8fdf9aec78571da83f4fba
Andrew Rogala <adrogala87@gmail.com>
2019-01-21 14:52:46 -0500

    add star to sass skillset

--- src/pages/about.js
+++ src/pages/about.js
@@ -73,7 +73,7 @@ class About extends React.Component {
 							<div className="skills__imgdiv" data-aos="slide-left" data-aos-delay="350" data-aos-easing="ease-in-out-back"><img src={Star} alt="Star"/></div>
 							<div className="skills__imgdiv" data-aos="slide-left" data-aos-delay="350" data-aos-easing="ease-in-out-back"><img src={Star} alt="Star"/></div>
 							<div className="skills__imgdiv" data-aos="slide-left" data-aos-delay="350" data-aos-easing="ease-in-out-back"><img src={Star} alt="Star"/></div>
-							<div className="skills__imgdiv" data-aos="slide-left" data-aos-delay="350" data-aos-easing="ease-in-out-back"></div>
+							<div className="skills__imgdiv" data-aos="slide-left" data-aos-delay="350" data-aos-easing="ease-in-out-back"><img src={Star} alt="Star"/></div>
 							<div className="skills__imgdiv" data-aos="slide-left" data-aos-delay="350" data-aos-easing="ease-in-out-back"></div>
 						</div>
 						<div className="skills__imgbox" data-aos="slide-left" data-aos-easing="ease-in-out-back">

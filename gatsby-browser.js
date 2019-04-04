0d22c08cc54b37cbda89f3bb6e074591a4aac924
ARogala <adrogala87@gmail.com>
2019-04-04 10:34:38 -0400

    edit type text on homepage

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 
 exports.onRouteUpdate = ({location}) => {
 	if(location.pathname === '/') {
-		const text = [`Hi, My name is Andrew! I am a Front-End Web developer specializing in React JavaScript and the JAM (JavaScript API's and Markdown) Stack. I love making custom web sites, blogs, and web apps. Check out some of my work below!`];
+		const text = [`Hi, My name is Andrew! I am a Full-Stack Web developer specializing in React, JavaScript, and NodeJS. I love making custom web sites, blogs, and web apps. Check out some of my work below!`];
 		const paragraph = document.getElementById('typewriter');
 
 		let i = 0;
